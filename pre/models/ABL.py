0c08ec2b02a696a991b790c58296c9e8d6f9d45e
ventr1c <mfl5681@psu.edu>
2024-05-19 20:18:37 -0400

    implement ABL

--- pre/models/ABL.py
+++ pre/models/ABL.py
@@ -12,21 +12,23 @@ from copy import deepcopy
 import heuristic_selection as hs
 
 class ABL(nn.Module):
-    def __init__(self, args, model_name, model, isolation_ratio, threshold, isolate_epoch, device):
+    def __init__(self, args, model_name, model, isolation_ratio, threshold, loss_increse_threshold, isolate_epoch, device):
         super(ABL, self).__init__()
         self.args = args
         self.model_name = model_name
         self.model = model
+        self.sur_model = deepcopy(self.model)
         self.device = device
         self.threshold = threshold
+        self.loss_increse_threshold = loss_increse_threshold
         self.isolate_epoch = isolate_epoch
         self.isolation_ratio = isolation_ratio  # 0.01
 
     def compute_loss_value(self,idx_train):
         criterion = nn.NLLLoss(reduction = 'none').to(self.device)
-        self.model.eval()
+        self.sur_model.eval()
         with torch.no_grad():
-            output = self.forward(self.features, self.edge_index, self.edge_weight)
+            output = self.forward_sur(self.features, self.edge_index, self.edge_weight)
             losses_record = criterion(output[idx_train],self.labels[idx_train])
         idx_losses_record = np.argsort(np.array(losses_record.detach().cpu()))
         
@@ -46,6 +48,15 @@ class ABL(nn.Module):
             output, x = self.model.forward(features, edge_index, edge_weight)
         elif(self.model_name in ['GAT','GraphSAGE','GIN']):
             output= self.model.forward(features, edge_index, edge_weight)
+        else:
+            raise NotImplementedError("No implemented model")
+        return output
+
+    def forward_sur(self, features, edge_index, edge_weight):
+        if(self.model_name == 'GCN'):
+            output, x = self.sur_model.forward(features, edge_index, edge_weight)
+        elif(self.model_name in ['GAT','GraphSAGE','GIN']):
+            output= self.sur_model.forward(features, edge_index, edge_weight)
         else:
             raise NotImplementedError("No implemented model")
         return output
@@ -79,7 +90,6 @@ class ABL(nn.Module):
 
         self.labels = labels.to(self.device)
         # self.prob_drop = prob_drop
-
         if idx_val is None:
             self._train_without_val(self.labels, idx_train, train_iters, verbose)
         else:
@@ -107,27 +117,28 @@ class ABL(nn.Module):
                 threshold = self.threshold
                 optimizer.zero_grad()
                 output = self.forward(self.features, self.edge_index, self.edge_weight)
-                loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+                loss_train = F.nll_loss(output[idx_train], labels[idx_train],reduction = 'none')
                 # idx_isolated, idx_clean = self.isolate_data(idx_train)
                 loss_train = torch.sign(loss_train - threshold) * loss_train
-                
                 loss_train.backward()
                 optimizer.step()
             else:
 
-                optimizer.zero_grad()
-
                 # rs_edge_index, rs_edge_weight = self.sample_noise_all(self.prob_drop, self.edge_index, self.edge_weight, self.device)
 
                 output = self.forward(self.features, self.edge_index, self.edge_weight)
-
+                optimizer.zero_grad()
                 loss_train_isolated = -1 * F.nll_loss(output[idx_isolated], labels[idx_isolated])
+                loss_train_isolated.backward()
+                print('Epoch {}, loss_train_isolated: {}'.format(i, loss_train_isolated.item()))
+                optimizer.step()
+
+                optimizer.zero_grad()
                 loss_train_clean = F.nll_loss(output[idx_clean], labels[idx_clean])
-
-                loss_train = loss_train_clean + loss_train_isolated
-
-                loss_train = F.nll_loss(output[idx_train], labels[idx_train])
-                loss_train.backward()
+                # loss_train = loss_train_clean + loss_train_isolated
+                # loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+                loss_train_clean.backward()
+                print('Epoch {}, loss_train_clean: {}'.format(i, loss_train_clean.item()))
                 optimizer.step()
 
                 if verbose and i % 10 == 0:
@@ -139,52 +150,76 @@ class ABL(nn.Module):
         self.output = output
 
     def _train_with_val(self, labels, idx_train, idx_val, train_iters, verbose):
+        
         if verbose:
             print('=== training gnn model ===')
         optimizer = optim.Adam(self.model.parameters(), lr=self.model.lr, weight_decay=self.model.weight_decay)
-
+        optimizer_sur = optim.Adam(self.sur_model.parameters(), lr=self.model.lr, weight_decay=self.model.weight_decay)
         best_loss_val = 100
         best_acc_val = 0
 
-        isolate_epoch = 40
+        isolate_epoch = self.isolate_epoch
 
         for i in range(train_iters):
 
-            self.model.train()
-            if( i == isolate_epoch):
-                idx_isolated, idx_clean = self.isolate_data(idx_train)
+            # if( i == isolate_epoch):
+            #     idx_isolated, idx_clean = self.isolate_data(idx_train)
                 
             if(i < isolate_epoch):
-                threshold = self.threshold
-                optimizer.zero_grad()
-                output = self.forward(self.features, self.edge_index, self.edge_weight)
-                loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+                self.sur_model.train()
+                self.model.eval()
+                optimizer_sur.zero_grad()
+                output = self.forward_sur(self.features, self.edge_index, self.edge_weight)
+                loss_train = F.nll_loss(output[idx_train], labels[idx_train],reduction = 'none')
                 # idx_isolated, idx_clean = self.isolate_data(idx_train)
-                loss_train = torch.sign(loss_train - threshold) * loss_train
-                
+                # print(loss_train[-31:])
+                # print(loss_train[:-31])
+                # loss_train = torch.sign(loss_train - self.threshold) * loss_train
+                # # print(loss_train[-31:])
+                loss_train = torch.mean(loss_train)
                 loss_train.backward()
-                optimizer.step()
+                optimizer_sur.step()
+                idx_isolated, idx_clean = self.isolate_data(idx_train)
             else:
-
-                optimizer.zero_grad()
+                self.sur_model.eval()
+                self.model.train()
 
                 # rs_edge_index, rs_edge_weight = self.sample_noise_all(self.prob_drop, self.edge_index, self.edge_weight, self.device)
 
+                optimizer.zero_grad()
                 output = self.forward(self.features, self.edge_index, self.edge_weight)
+                loss_train_isolated = -1 * F.nll_loss(output[idx_isolated], labels[idx_isolated], reduction = 'none')
+                loss_train_isolated = torch.relu(loss_train_isolated + self.loss_increse_threshold)
+                loss_train_isolated = torch.mean(loss_train_isolated)
 
-                loss_train_isolated = -1 * F.nll_loss(output[idx_isolated], labels[idx_isolated])
-                loss_train_clean = F.nll_loss(output[idx_clean], labels[idx_clean])
-
-                loss_train = loss_train_clean + loss_train_isolated
-
-                loss_train = F.nll_loss(output[idx_train], labels[idx_train])
-                loss_train.backward()
+                # loss_train_isolated = -1 * loss_train_isolated
+                loss_train_isolated.backward()
+                print('Epoch {}, loss_train_isolated: {}'.format(i, loss_train_isolated.item()))
                 optimizer.step()
+
+                optimizer.zero_grad()
+                output = self.forward(self.features, self.edge_index, self.edge_weight)
+                loss_train_clean = F.nll_loss(output[idx_clean], labels[idx_clean])
+                # loss_train = loss_train_clean + loss_train_isolated
+                # loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+                loss_train_clean.backward()
+                print('Epoch {}, loss_train_clean: {}'.format(i, loss_train_clean.item()))
+                optimizer.step()
+
+                # optimizer.zero_grad()
+                # output = self.forward(self.features, self.edge_index, self.edge_weight)
+
+                # loss_train_isolated = -1 * F.nll_loss(output[idx_isolated], labels[idx_isolated])
+                # loss_train_clean = F.nll_loss(output[idx_clean], labels[idx_clean])
+                # loss_train = loss_train_clean + loss_train_isolated
+
+                # loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+                # loss_train.backward()
+                # optimizer.step()
 
 
 
                 self.model.eval()
-                # rs_edge_index, rs_edge_weight = self.sample_noise_all(self.prob_drop, self.edge_index, self.edge_weight, self.device)
                 output = self.forward(self.features, self.edge_index, self.edge_weight)
                 loss_val = F.nll_loss(output[idx_val], labels[idx_val])
                 acc_val = utils.accuracy(output[idx_val], labels[idx_val])
